9481e6c0a21c65e363b0dbe29df0fdb8acd2774d
Antoine Hérault <antoine.herault@gmail.com>
2011-06-30 22:11:46 +0200

    Add more tests for the exposer

--- tests/MediaExposer/ExposerTest.php
+++ tests/MediaExposer/ExposerTest.php
@@ -4,6 +4,219 @@ namespace MediaExposer;
 
 class ExposerTest extends \PHPUnit_Framework_TestCase
 {
+    public function testConstruct()
+    {
+        $exposer = new Exposer();
+
+        $this->assertAttributeEquals(null, 'baseUrl', $exposer, 'The base url should be null by default.');
+
+        $exposer = new Exposer('http://the-base.url');
+
+        $this->assertAttributeEquals('http://the-base.url', 'baseUrl', $exposer, 'The base url should have the value providen in the constructor.');
+    }
+
+    public function testSetBaseUrl()
+    {
+        $exposer = new Exposer('http://the-old-base.url');
+        $exposer->setBaseUrl('http://the-new-base.url');
+
+        $this->assertAttributeEquals('http://the-new-base.url', 'baseUrl', $exposer, 'The base url should have the new defined value.');
+    }
+
+    public function testGetPath()
+    {
+        $exposer = new Exposer();
+        $exposer->addResolver($a = $this->getPathResolverMock());
+        $exposer->addResolver($b = $this->getPathResolverMock());
+        $exposer->addResolver($c = $this->getPathResolverMock());
+
+        $a
+            ->expects($this->once())
+            ->method('supports')
+            ->with($this->equalTo('TheMedia'), $this->equalTo(array('foo' => 'bar')))
+            ->will($this->returnValue(false))
+        ;
+        $a
+            ->expects($this->never())
+            ->method('getPath')
+        ;
+
+        $b
+            ->expects($this->once())
+            ->method('supports')
+            ->with($this->equalTo('TheMedia'), $this->equalTo(array('foo' => 'bar')))
+            ->will($this->returnValue(true))
+        ;
+        $b
+            ->expects($this->once())
+            ->method('getPath')
+            ->with($this->equalTo('TheMedia'), $this->equalTo(array('foo' => 'bar')))
+            ->will($this->returnValue('/path/of/the/media'))
+        ;
+
+        $c
+            ->expects($this->never())
+            ->method('supports')
+        ;
+
+        $this->assertEquals(
+            '/path/of/the/media',
+            $exposer->getPath('TheMedia', array('foo' => 'bar')),
+            'Should return the value providen by the second resolver.'
+        );
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testGetPathWhenThereIsNotSupportingResolver()
+    {
+        $exposer = new Exposer();
+        $exposer->addResolver($a = $this->getPathResolverMock());
+        $exposer->addResolver($b = $this->getPathResolverMock());
+
+        $a
+            ->expects($this->once())
+            ->method('supports')
+            ->with($this->equalTo('TheMedia'), $this->equalTo(array('foo' => 'bar')))
+            ->will($this->returnValue(false))
+        ;
+
+        $b
+            ->expects($this->once())
+            ->method('supports')
+            ->with($this->equalTo('TheMedia'), $this->equalTo(array('foo' => 'bar')))
+            ->will($this->returnValue(false))
+        ;
+
+        $exposer->getPath('TheMedia', array('foo' => 'bar'));
+    }
+
+    public function testGetSource()
+    {
+        $exposer = new Exposer();
+        $exposer->addResolver($a = $this->getSourceResolverMock());
+        $exposer->addResolver($b = $this->getSourceResolverMock());
+        $exposer->addResolver($c = $this->getSourceResolverMock());
+
+        $a
+            ->expects($this->once())
+            ->method('supports')
+            ->with($this->equalTo('TheMedia'), $this->equalTo(array('foo' => 'bar')))
+            ->will($this->returnValue(false))
+        ;
+        $a
+            ->expects($this->never())
+            ->method('getSource')
+        ;
+
+        $b
+            ->expects($this->once())
+            ->method('supports')
+            ->with($this->equalTo('TheMedia'), $this->equalTo(array('foo' => 'bar')))
+            ->will($this->returnValue(true))
+        ;
+        $b
+            ->expects($this->once())
+            ->method('getSource')
+            ->with($this->equalTo('TheMedia'), $this->equalTo(array('foo' => 'bar')))
+            ->will($this->returnValue('/source/of/the/media'))
+        ;
+
+        $c
+            ->expects($this->never())
+            ->method('supports')
+        ;
+
+        $this->assertEquals(
+            '/source/of/the/media',
+            $exposer->getSource('TheMedia', array('foo' => 'bar')),
+            'Should return the value providen by the second resolver.'
+        );
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testGetSourceWithNoSupportingResolver()
+    {
+        $exposer = new Exposer();
+        $exposer->addResolver($a = $this->getSourceResolverMock());
+        $exposer->addResolver($b = $this->getSourceResolverMock());
+
+        $a
+            ->expects($this->once())
+            ->method('supports')
+            ->with($this->equalTo('TheMedia'), $this->equalTo(array('foo' => 'bar')))
+            ->will($this->returnValue(false))
+        ;
+
+        $b
+            ->expects($this->once())
+            ->method('supports')
+            ->with($this->equalTo('TheMedia'), $this->equalTo(array('foo' => 'bar')))
+            ->will($this->returnValue(false))
+        ;
+
+        $exposer->getSource('TheMedia', array('foo' => 'bar'));
+    }
+
+    public function testGetSourceWithForceAbsoluteAndAnAbsoluteResolver()
+    {
+        $exposer = new Exposer('http://the-host');
+        $exposer->addResolver($a = $this->getSourceResolverMock());
+
+        $a
+            ->expects($this->once())
+            ->method('supports')
+            ->will($this->returnValue(true))
+        ;
+        $a
+            ->expects($this->once())
+            ->method('getSource')
+            ->will($this->returnValue('http://the-absolute/source'))
+        ;
+        $a
+            ->expects($this->once())
+            ->method('getSourceType')
+            ->will($this->returnValue(SourceResolver::TYPE_ABSOLUTE))
+        ;
+
+        $this->assertEquals(
+            'http://the-absolute/source',
+            $exposer->getSource('foo', array(), true),
+            'Should NOT prepend the base url to the absolute resolver\'s source when $forceAbsolute is set to TRUE.'
+        );
+    }
+
+    public function testGetSourceWithForceAbsoluteAndAnRelativeResolver()
+    {
+        $exposer = new Exposer('http://the-host');
+        $exposer->addResolver($a = $this->getSourceResolverMock());
+
+        $a
+            ->expects($this->once())
+            ->method('supports')
+            ->will($this->returnValue(true))
+        ;
+        $a
+            ->expects($this->once())
+            ->method('getSource')
+            ->will($this->returnValue('/the/relative/source'))
+        ;
+        $a
+            ->expects($this->once())
+            ->method('getSourceType')
+            ->will($this->returnValue(SourceResolver::TYPE_RELATIVE))
+        ;
+
+        $this->assertEquals(
+            'http://the-host/the/relative/source',
+            $exposer->getSource('foo', array(), true),
+            'Should prepend the base url to the relative resolver\'s source when $forceAbsolute is set to TRUE.'
+        );
+    }
+
     public function testGetResolvers()
     {
         $exposer = new Exposer();
@@ -104,6 +317,33 @@ class ExposerTest extends \PHPUnit_Framework_TestCase
             iterator_to_array($exposer->getSortedPathResolvers(), false),
             'Should return the path resolvers sorted by priority from the smallest to the largest).'
         );
+    }
+
+    public function testAbsolutify()
+    {
+        $exposer = new Exposer('http://the-host');
+
+        $r = new \ReflectionMethod($exposer, 'absolutify');
+        $r->setAccessible(true);
+
+        $this->assertEquals(
+            'http://the-host/the/relative/source',
+            $r->invokeArgs($exposer, array('/the/relative/source')),
+            'Should prepend the base url to the given relative source.'
+        );
+    }
+
+    /**
+     * @expectedException LogicException
+     */
+    public function testAbsolutifyWhenTheBaseUrlIsNotDefined()
+    {
+        $exposer = new Exposer();
+
+        $r = new \ReflectionMethod($exposer, 'absolutify');
+        $r->setAccessible(true);
+
+        $r->invokeArgs($exposer, array('/the/relative/source'));
     }
 
     private function getSourceResolverMock()
